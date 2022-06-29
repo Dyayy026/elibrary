bb239e3c0af55d0b4fd01402cd5fb4524b07c1a0
Arjay Andal <arjayandal93@gmail.com>
2022-06-29 11:33:00 +0800

    Addrd: AAreset_pass.php

--- AAreset_pass.php
+++ AAreset_pass.php
@@ -1,91 +1,5 @@
 <?php require('conn.php'); ?>
 
-<?php
-require('includes/PHPMailer.php');
-require('includes/SMTP.php');
-require('includes/Exception.php');
-//define name spaces
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-if(isset($_REQUEST['send']))
-{
-    $email = $_POST['to'];
-    $sql = "SELECT email FROM u_details WHERE email = '$email' And date_added IS NOT NULL";
-    $result = $conn->query($sql);
-    $row = $result->fetch_assoc();
-
-    if ($row) {
-        ?>
-       
-        <script>
-        alert("<?php echo 'OTP has been sent to your email'?>");
-        </script>
-         echo '<script>window.location="AAreset_pass.php"</script>';
-        
-    </script>
-    <?php
-    extract($_REQUEST);
-    // extract($_FILES);
-    // extract($file);
-    $otp = rand(100000,999999);
-    $_SESSION['otp'] = $otp;
-    $email = $_SESSION['to'];
-    
-    //create a instance phpmailer
-    $mail = new PHPMailer();
-    //set mailer to use smtp
-    $mail->isSMTP();
-    //define smtp host
-    $mail->Host = "smtp.gmail.com";
-    //enable smtp authentication
-    $mail->SMTPAuth = "true";
-    //set type of encryption (ssl/tls)
-    $mail->SMTPSecure = "tls";
-    //set port to connect smtp
-    $mail->Port = "587";
-    //set gmail user
-    $mail->Username = "forcapstone312"; //before use see full video
-    //set gmail password
-    $mail->Password = "towfglvgxvohaxqa"; // see setting in youtube video working perfect
-    //set gmail subject
-    $mail->Subject = "Your verification code";
-    //set sender email
-    $mail->setFrom('forcapstone312@gmail.com', 'OTP Verification'); //setting good OTP verification as subject
-    //email body
-    $mail->Body="Dear user, we have receive a password reset request. Here is your OTP $otp";
-
-    //add recipient
-    $mail->addAddress($to);
-    // $mail->addCC($CC);
-    // $mail->addCC();
-    // $mail->addBCC($BCC);
-    // $mail->addAttachment($name);
-
-    //closing smtp connection
-    if($mail->Send())
-    {
-    $msg= "Email Sent";
-    }
-    else
-    {
-        $msg = "Email does not exist";
-    }
-    $mail->smtpClose();   
-        }else {
-            ?>
-            <script>
-            alert("<?php echo 'Email not found or your account has not been approved yet'?>");
-            header('Location: forgotPass.php');
-        </script>
-        <?php
-        }
-    }
-    ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -93,7 +7,7 @@ if(isset($_REQUEST['send']))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forgot Password</title>
+    <title>Reset Password</title>
     <!-- links in one  -->
     <?php include 'templates/links.php' ?>
     <link rel="stylesheet" href="css/footer.css">
@@ -148,31 +62,31 @@ if(isset($_REQUEST['send']))
                 <!-- login wrap start -->
                 <div class="login-wrap">
                     <font face="rockwell">
-                        <h1>Forgot Password</h1>
+                        <h1>Reset Password</h1>
                     </font>
                     <form action="#" method="POST">
                         <div class="form-group">
-                            <label for="text" for="email">Email</label>
-                            <input type="email" class="form-control" placeholder="Enter your email address" name="to" required="">
+                            <label for="text" for="email">Enter New Password.</label>
+                            <input type="text" class="form-control" placeholder="School ID No." name="pword1" required="">
                         </div>
-                        <p><i> OTP will be send to the email you provided.</i></p>
 
                         <div class="form-group">
-                            <button type="submit" class="form-control btn btn-dark submit button two" name="send"><span>Send OTP</span> </button>
+                            <label class="label" for="password">Confirm Password</label>
+                            <input type="password" class="form-control" placeholder="Password" name="pword2" required="">
+                        </div>
+                        <p class=""><a href="AAforgot_pass.php">Back to forgot password?</a> </p>
+
+                        <div class="form-group">
+                            <button type="submit" class="form-control btn btn-dark submit button two" name="reset_password"><span>Submit</span> </button>
                         </div>
                     </form>
 
                     <hr>
-                    <p class="text-center"><a href="login.php">Back to Login...</a> </p>
                 </div> <!-- login wrap end -->
             </div>
             <!--form box end-->
         </div>
         <!--right side end xontext box-->
-
-
-
-
 
     </section>
 
@@ -186,3 +100,48 @@ if(isset($_REQUEST['send']))
 
 </html>
 
+<?php
+    if(isset($_POST["reset_password"])){
+
+		
+        $psw = $_POST["pword1"];
+		$psw2 = $_POST["pword2"];
+        $email = $_SESSION['to'];
+      
+
+		if($psw == $psw2){
+
+			$sql = mysqli_query($conn, "SELECT * FROM u_details WHERE email='$email' AND date_added IS NOT NULL");
+			$query = mysqli_num_rows($sql);
+			  $fetch = mysqli_fetch_assoc($sql);
+	
+			if($email){
+				$hash = md5($psw);
+				$new_pass = $hash;
+				mysqli_query($conn, "UPDATE u_details SET pword='$new_pass' WHERE email='$email'");
+				?>
+				<script>
+					alert("<?php echo "Your password reset is succesful"?>");
+					window.location.replace("login.php");
+					
+				</script>
+				<?php
+			}else{
+				?>
+				<script>
+					alert("<?php echo "Please try again"?>");
+				</script>
+				<?php
+			}
+		}
+		else{
+			?>
+				<script>
+					alert("<?php echo "Passwords did not match!"?>");
+				</script>
+				<?php
+		}
+
+    }
+
+?>
